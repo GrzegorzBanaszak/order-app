7cf950bb663c68f7fd651a701c7f22d6bbb3fb91
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-08-15 10:55:42 +0200

    Adding global interfaces

---
+++ backend/src/common/interfaces/global.d.ts
@@ -0,0 +1,41 @@
+export {};
+
+declare global {
+  interface Order {
+    id: string;
+    phoneNumber: number;
+    customer: Customer;
+    orderData: Date;
+    commoditys: Commodity[];
+    status: string;
+    supplayer: Supplier;
+    advance: number;
+  }
+
+  interface Customer {
+    id: string;
+    name: string;
+    company: Company;
+    phoneNumber: number;
+  }
+
+  interface Company {
+    id: string;
+    name: string;
+    nip: string;
+  }
+
+  interface Supplier {
+    id: string;
+    name: string;
+    price: number;
+  }
+
+  interface Commodity {
+    id: string;
+    name: string;
+    description: string;
+    quantity: number;
+    price: number;
+  }
+}
