ffb616bebbab8c28977ae14f6f006c9c5432a1a3
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-08-25 20:26:26 +0200

    Adding router and simple routes to test

--- vue-client/.eslintrc.js
+++ vue-client/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["plugin:vue/vue3-recommended"],
-  rules: {},
+  rules: { "vue/multi-word-component-names": "off" },
   parserOptions: {
     parser: "@babel/eslint-parser",
     requireConfigFile: false, // <== ADD THIS
