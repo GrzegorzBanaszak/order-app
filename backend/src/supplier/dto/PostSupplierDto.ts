f598b4694a885d72d47c4eda4ba644e5b8c74c9b
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-10-03 20:35:56 +0200

    Change default error messages for supplier

--- backend/src/supplier/dto/PostSupplierDto.ts
+++ backend/src/supplier/dto/PostSupplierDto.ts
@@ -1,11 +1,11 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class PostSupplierDto {
     @IsString()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Podaj nazwę dostawcy' })
     name: string;
 
     @IsNumber()
-    @IsNotEmpty()
+    @Min(1, { message: 'Podaj cene dostawy' })
     price: number;
 }
