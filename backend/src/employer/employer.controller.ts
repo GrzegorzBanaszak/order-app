2d06b91277394032fa3c4b4b6222b39e91e6662a
Grzegorz Banaszak <grzegorz.banaszak95@gmail.com>
2022-11-14 13:52:26 +0100

    Get employers by user id

--- backend/src/employer/employer.controller.ts
+++ backend/src/employer/employer.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    HttpException,
+    HttpStatus,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { ObjectId } from 'mongodb';
+import { EmployerService } from './employer.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('employer')
-export class EmployerController {}
+export class EmployerController {
+    constructor(private employerService: EmployerService) {}
+
+    @Get()
+    async getAll(@Req() req: any) {
+        await this.employerService.getAll(
+            ObjectId.createFromHexString(req.user.userId),
+        );
+    }
+}
